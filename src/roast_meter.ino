25274de4eff3fcb3fef082c213ad2504a11fe168
Juztin <iustinian.nita@gmail.com>
2023-03-11 23:21:07 +0200

    Update roast_meter.ino
    Updated code calibrated to match better the agtron gourmet readings. Updated information display for better overall experience.

--- src/roast_meter.ino
+++ src/roast_meter.ino
@@ -21,67 +21,48 @@ String multiplyChar(char c, int n) {
 }
 
 void displayMeasurement(int rLevel) {
+  oled.clear(PAGE);
   oled.setCursor(0, 0);
 
-  String strLevel = String(rLevel);
-  char pad_char = ' ';
-
-  String pad_str = multiplyChar(pad_char, 3);
-  oled.print(pad_str);
-  oled.print(strLevel);
-  oled.print(pad_str);
-
-  oled.setCursor(0, 16);
-
-  if (rLevel >= 110) {
-    oled.print("exlight");
-    oled.print(multiplyChar(pad_char, 1));
+  int calibratedReading = f(rLevel);
+  int centerPadding = 4 - String(calibratedReading).length();
+  String paddingText = multiplyChar(' ', centerPadding);
+  
+  if (rLevel == 0) {
+    oled.setFontType(1);
+    oled.print("Please load   sample!");
+    oled.display();
+    return;
   }
+  
+  oled.setFontType(3);
+  oled.print(paddingText);
+  oled.print(calibratedReading);
 
-  if (rLevel >= 96 && rLevel < 110) {
-    oled.print("vlight");
-    oled.print(multiplyChar(pad_char, 2));
-  }
-
-  if (rLevel >= 90 && rLevel < 95) {
-    oled.print("light");
-    oled.print(multiplyChar(pad_char, 3));
-  }
-
-  if (rLevel >= 81 && rLevel < 90) {
-    oled.print("mlight");
-    oled.print(multiplyChar(pad_char, 2));
-  }
-
-  if (rLevel >= 71 && rLevel < 80) {
-    oled.print("medium");
-    oled.print(multiplyChar(pad_char, 2));
-  }
-
-  if (rLevel >= 61 && rLevel < 70) {
-    oled.print("dark");
-    oled.print(multiplyChar(pad_char, 4));
-  }
-
-  if (rLevel < 61) {
-    oled.print("vdark");
-    oled.print(multiplyChar(pad_char, 3));
-  }
+  Serial.println("real:" + String(rLevel));
+  Serial.println("agtron:" + String(calibratedReading));
+  Serial.println("===========================");
 
   oled.display();
 }
 
+int f(int x) {
+  int intersectionPoint = 112;
+  float deviation = 0.164;
+
+  return x - (intersectionPoint - x) * deviation;
+}
+
 void setup() {
   Serial.begin(9600);
-  
+
   Wire.begin();
-  oled.begin();    // Initialize the OLED
-  oled.clear(ALL); // Clear the display's internal memory
-  oled.display();  // Display what's in the buffer (splashscreen)
-
-  delay(1000); // Delay 1000 ms
-
+  oled.begin();     // Initialize the OLED
+  oled.clear(ALL);  // Clear the display's internal memory
   oled.clear(PAGE); // Clear the buffer.
+
+  delay(100);       // Delay 100 ms
+  oled.setFontType(3);
 
   // Initialize sensor
   if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false)  //Use default I2C port, 400kHz speed
@@ -92,26 +73,24 @@ void setup() {
   }
 
   //Setup to sense up to 18 inches, max LED brightness
-  byte ledBrightness = 0x1F;  //Options: 0=Off to 255=50mA
-  byte sampleAverage = 16;     //Options: 1, 2, 4, 8, 16, 32
+  byte ledBrightness = 0x24;  //Options: 0=Off to 255=50mA
+  byte sampleAverage = 32;    //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2;           //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 400;       //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int sampleRate = 3200;      //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 411;       //Options: 69, 118, 215, 411
   int adcRange = 4096;        //Options: 2048, 4096, 8192, 16384
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);  //Configure sensor with these settings
 
   //particleSensor.setPulseAmplitudeRed(0);    //Turn off Red LED
-  //particleSensor.setPulseAmplitudeGreen(0);  //Turn off Green LED
+  particleSensor.setPulseAmplitudeGreen(0);  //Turn off Green LED
 
-  //Take an average of IR readings at power up
-  unblockedValue = 0;
-  for (byte x = 0; x < 32; x++) {
-    unblockedValue += particleSensor.getIR();  //Read the IR value
-  }
-  unblockedValue /= 32;
 
+  Serial.print(unblockedValue);
   startTime = millis();
+
+  // Update to ignore readings under 30.000
+  unblockedValue = 30000;
 }
 
 void loop() {
@@ -119,11 +98,10 @@ void loop() {
   long currentDelta = particleSensor.getIR() - unblockedValue;
 
   if (currentDelta > (long)100) {
-    Serial.print("IR - ");
-    int rLevel = int(particleSensor.getIR() / 1000);
-    Serial.print(rLevel);
+    int rLevel = int(particleSensor.getIR()) / 1000;
     displayMeasurement(rLevel);
-    Serial.println("");
+  } else {
+    displayMeasurement(0);
   }
   delay(100);
 }
