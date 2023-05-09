52dc33538270c56f06b4aca5e65a8c175c4ca928
Chawasit Tengtrairatana <chawasit555@gmail.com>
2023-05-09 22:40:58 +0700

    add arduinoble and refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,108 @@
 #include <Wire.h>
 #include "MAX30105.h"
 #include <SFE_MicroOLED.h>
+#include <ArduinoBLE.h>
+
+
+// -- Constant Values --
 
 #define PIN_RESET 9
 #define DC_JUMPER 1
 
+// -- End Constant Values --
+
+
+// -- Global Variables --
+
+long unblockedValue;    //Average IR at power up
 MAX30105 particleSensor;
 MicroOLED oled(PIN_RESET, DC_JUMPER);
 
-long unblockedValue;    //Average IR at power up
+// -- End Global Variables --
 
-String multiplyChar(char c, int n) {
-  String result = "";
-  for (int i = 0; i < n; i++) {
-    result += c;
+
+// -- Global Configs --
+
+// The variable below calibrates the LED output on your hardware.  
+byte ledBrightness;
+byte sampleAverage = 4;   //Options: 1, 2, 4, 8, 16, --32--
+byte ledMode = 2;         //Options: 1 = Red only, --2 = Red + IR--, 3 = Red + IR + Green
+int sampleRate = 50;      //Options: 50, 100, 200, 400, 800, 1000, 1600, --3200--
+int pulseWidth = 411;     //Options: 69, 118, 215, --411--
+int adcRange = 4096;      //Options: 2048, --4096--, 8192, 16384
+
+// -- End Global Configs --
+
+
+// -- Sub Routine Headers --
+
+void displayMeasurement(int rLevel);
+
+// -- End Sub Routine Headers --
+
+
+// -- BLE Function Headers --
+
+
+// -- End BLE Function Headers --
+
+
+// -- Utility Function Headers --
+
+String multiplyChar(char c, int n);
+int mapIRToAgtron(int x);
+
+// -- End Utillity Function Headers --
+
+
+// -- Main Process --
+void setup() {
+  Serial.begin(9600);
+
+  Wire.begin();
+  oled.begin();      // Initialize the OLED
+  oled.clear(ALL);   // Clear the display's internal memory
+  oled.clear(PAGE);  // Clear the buffer.
+
+  delay(100);  // Delay 100 ms
+  oled.setFontType(3);
+
+  // Initialize sensor
+  if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false)  //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1)
+      ;
   }
-  return result;
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);  //Configure sensor with these settings
+
+  // Update to ignore readings under 30.000
+  unblockedValue = 30000;
 }
 
-int f(int x) {
-  int intersectionPoint = 117;
-  float deviation = 0.165;
+void loop() {
+  int rLevel = particleSensor.getIR();
+  long currentDelta = rLevel - unblockedValue;
 
-  return round(x - (intersectionPoint - x) * deviation);
+  if (currentDelta > (long)100) {    
+    displayMeasurement(rLevel/1000);
+  } else {    
+    displayMeasurement(0);
+  }
+  delay(100);
 }
+
+// -- End Main Process --
+
+
+// Sub Routines
 
 void displayMeasurement(int rLevel) {
   oled.clear(PAGE);
   oled.setCursor(0, 0);
 
-  int calibratedReading = f(rLevel);
+  int calibratedReading = mapIRToAgtron(rLevel);
   int centerPadding = 4 - String(calibratedReading).length();
   String paddingText = multiplyChar(' ', centerPadding);
 
@@ -52,48 +125,24 @@ void displayMeasurement(int rLevel) {
   oled.display();
 }
 
-void setup() {
-  Serial.begin(9600);
+// Sub Routines
 
-  Wire.begin();
-  oled.begin();      // Initialize the OLED
-  oled.clear(ALL);   // Clear the display's internal memory
-  oled.clear(PAGE);  // Clear the buffer.
 
-  delay(100);  // Delay 100 ms
-  oled.setFontType(3);
+// Utillity Functions
 
-  // Initialize sensor
-  if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false)  //Use default I2C port, 400kHz speed
-  {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
-    while (1)
-      ;
+String multiplyChar(char c, int n) {
+  String result = "";
+  for (int i = 0; i < n; i++) {
+    result += c;
   }
-
-  // The variable below calibrates the LED output on your hardware.  
-  byte ledBrightness = 34;
-  
-  byte sampleAverage = 4;   //Options: 1, 2, 4, 8, 16, --32--
-  byte ledMode = 2;         //Options: 1 = Red only, --2 = Red + IR--, 3 = Red + IR + Green
-  int sampleRate = 50;      //Options: 50, 100, 200, 400, 800, 1000, 1600, --3200--
-  int pulseWidth = 411;     //Options: 69, 118, 215, --411--
-  int adcRange = 4096;      //Options: 2048, --4096--, 8192, 16384
-
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);  //Configure sensor with these settings
-
-  // Update to ignore readings under 30.000
-  unblockedValue = 30000;
+  return result;
 }
 
-void loop() {
-   int rLevel = particleSensor.getIR();
-   long currentDelta = rLevel - unblockedValue;
+int mapIRToAgtron(int x) {
+  int intersectionPoint = 117;
+  float deviation = 0.165;
 
-  if (currentDelta > (long)100) {    
-    displayMeasurement(rLevel/1000);
-  } else {    
-    displayMeasurement(0);
-  }
-  delay(100);
+  return round(x - (intersectionPoint - x) * deviation);
 }
+
+// End Utillity Functions
